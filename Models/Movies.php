e442475002197c33d656d3f5cda12c7e2c09cb44
LuisJann <giannoneluigi10@gmail.com>
2022-12-11 14:10:12 +0100

    Add function "durationFilm"

--- Models/Movies.php
+++ Models/Movies.php
@@ -4,11 +4,24 @@ class Movies
     public $name;
     public $genre;
     public $duration;
+    private $durationFilm;
 
-    function __construct($name, $genre, $duration = "")
+    function __construct($name, $genre = null, $duration = "")
     {
         $this->name = $name;
         $this->genre = $genre;
         $this->duration = $duration;
+    }
+
+    public function getDuration()
+    {
+        return $this->durationFilm;
+    }
+
+    public function setDuration($duration)
+    {
+        if ($duration >= 100) {
+            $this->durationFilm = "long film";
+        }
     }
 };
--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 require_once __DIR__ . "/Models/Movies.php";
 
-$shutterIsland = new Movies("shutter island", "thriller", 120);
-var_dump($shutterIsland);
+// $shutterIsland = new Movies("shutter island", "thriller", 120);
+// var_dump($shutterIsland);
 
 $intoTheWild = new Movies("into the wild", "adventure");
 var_dump($intoTheWild);
+
+$shutterIsland = new Movies("Shutter Island");
+$shutterIsland->name = "Shutter Island";
+$shutterIsland->genre = "thriller";
+$shutterIsland->duration = 100;
+$shutterIsland->setDuration(100);
+
+var_dump($shutterIsland);
+var_dump($shutterIsland->getDuration());
